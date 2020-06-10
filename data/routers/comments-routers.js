a23a7c8db549dbef2fee079c198fbe5586739f5a
remoo1901 <remoo1901@hotmail.com>
2020-06-09 22:49:44 -0400

    mvp completed

--- data/routers/comments-routers.js
+++ data/routers/comments-routers.js
@@ -6,63 +6,54 @@ const router = express.Router();
 // GET request
 
 router.get("/api/posts/:id/comments", (req, res) => {
-    db.findById(req.params.id)
-    .then ((post) => {
-        if (!post) {
-            res.status(404).json({
-                message: "post with this ID  not found",
-            })
-        } else {
-            db.findCommentById(req.params.id)
-        .then((comment) => {
-          res.status(200).json(comment)
-        })
-        }
+  db.findById(req.params.id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({
+          message: "post with this ID  not found",
+        });
+      } else {
+        db.findCommentById(req.params.id).then((comment) => {
+          res.status(200).json(comment);
+        });
+      }
     })
     .catch((err) => {
-        console.log(err)
-        res.status(500).json({
-            message: "post with this ID  not found",
-          })
-    })
-  });
-
-
-
-  // POST request
-
-
-  router.post("/api/posts/:id/comments", (req, res) => {
-    db
-      .findById(req.params.id)
-      .then((post) => {
-        if (!post) {
-          res.status(404).json({
-            message: "The post with the specified ID does not exist.",
-          });
-        }
-  
-        if (!req.body.text) {
-          res.status(400).json({
-            errorMessage: "Please provide text for the comment.",
-          });
-        }
-  
-        db.insertComment({
-          text: req.params.text,
-          post_id: req.body.post_id,
-        });
-        if (comment) {
-          res.status(201).json(comment);
-        }
-      })
-  
-      .catch((err) => {
-        console.log(err);
-        res.status(500).json({
-          error: "There was an error while saving the comment to the database",
-        });
+      console.log(err);
+      res.status(500).json({
+        message: "post with this ID  not found",
       });
-  });
+    });
+});
+
+// POST request
+
+router.post("/api/posts/:id/comments", (req, res) => {
+  const comment = { ...req.body, post_id: req.params.id };
+
+  if (!req.params.id) {
+    res.status(404).json({
+      message: "The post with the specified ID does not exist.",
+    });
+  } else if (!req.body.text) {
+    res.status(400).json({
+      message: "Please provide text for the comment.",
+    });
+  } else {
+    db.insertComment(comment)
+    .then((comment) => {
+      res.status(201).json(comment);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        message: "post with this ID  not found",
+      });
+    });
+  }
+  
+});
 
 module.exports = router;
+
+ 
